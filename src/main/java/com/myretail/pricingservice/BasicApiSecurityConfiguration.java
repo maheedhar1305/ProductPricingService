53b26ef3b148d09db248f62e68298273a058b9a2
Maheedhar Gunasekharan <maheedhar.gunasekharan@randstadusa.com>
2019-04-01 20:50:21 -0400

    with password encoder

--- src/main/java/com/myretail/pricingservice/BasicApiSecurityConfiguration.java
+++ src/main/java/com/myretail/pricingservice/BasicApiSecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.myretail.pricingservice;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +20,7 @@ public class BasicApiSecurityConfiguration extends WebSecurityConfigurerAdapter 
         auth
           .inMemoryAuthentication()
           .withUser("admin")
-            .password("{noop}admin")
+            .password(encoder().encode("admin"))
             .roles("ADMIN");
     }
  
@@ -31,5 +34,10 @@ public class BasicApiSecurityConfiguration extends WebSecurityConfigurerAdapter 
         .and()
         .httpBasic();
     	
+    }
+    
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
